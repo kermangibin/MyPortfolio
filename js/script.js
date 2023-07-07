e3c4fab962dfeeb5c730f112e459d4b1d42d6b7e
kermangibin <kierbristol@gmail.com>
2023-07-07 20:30:01 +0800

    Success

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Virtual Assistant', 'Agency Owner', 'ComSci Student'],
+    strings: ['Virtual Assistant', 'Graphic Designer', 'ComSci Student'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
